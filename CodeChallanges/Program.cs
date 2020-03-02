4621b81c38b9647de14d00a186835a95bdb06bff
unknown <fortunato.mugnano88@gmail.com>
2020-03-02 17:12:35 -0600

    first commit

---
+++ CodeChallanges/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar)
+    {
+        /*
+         * Write your code here.
+         */
+
+        int n = ar.Length;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += ar[i];
+        return sum;
+
+    }
+
+    public static void Main()
+    {
+
+        int[] ar = { 12, 3, 4, 15 };
+
+
+        Console.Write($"Sum of given array is {simpleArraySum(ar)}");
+    }
+}
